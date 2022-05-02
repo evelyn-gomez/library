cef4535c0345995691089ae562650249a19314b0
Evelyn Gomez <evelyn.gomez15@gmail.com>
2022-05-01 20:08:23 -0700

    removed test book

--- script.js
+++ script.js
@@ -251,9 +251,9 @@ window.onclick= function(event) {
  */
 // renderExisitingBooksInLibrary(); 
 
-let TESTBOOK = new Book('test-title', 'test-author', 20, 'true');
+// let TESTBOOK = new Book('test-title', 'test-author', 20, 'true');
 
-addBookToBookShelf(TESTBOOK);
+// addBookToBookShelf(TESTBOOK);
 
 
   
